3c05a8222b53a404b2309f94d2b699cf3df87a3f
osasalohan <58654983+osasalohan@users.noreply.github.com>
2020-12-10 00:07:40 +0100

    allow cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const methodOverride = require("method-override");
 
 const authRoutes = require("./routes/auth");
@@ -15,6 +16,7 @@ const app = express();
 //express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(methodOverride("_method"));
 
 //available routes
